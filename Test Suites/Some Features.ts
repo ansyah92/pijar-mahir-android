6285d19ef4ddc5500bc9cc5189f8705d2b8e07ed
Ardi ansyah <ardiansyah@Ardis-MacBook-Pro.local>
2022-04-12 14:18:15 +0700

    android

---
+++ Test Suites/Some Features.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Some Features</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>1000</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>73713de3-fdb8-4735-9a7c-4f987be82e31</testSuiteGuid>
+   <testCaseLink>
+      <guid>c4e9ffa5-0f92-476a-9905-9dbe2dfa93bb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login valid username password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1c97b1b2-b37e-42fa-a51b-f2c25fe5a7e1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Search Course</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ae215ed5-0f43-4b88-994b-c30faf8333e1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Logout</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
